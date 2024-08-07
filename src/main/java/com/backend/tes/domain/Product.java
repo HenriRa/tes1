29db6082918ebd62252e55f7eb1dfa5cbfc92d9a
HenrikaRa <henrika.ragauske@gmail.com>
2024-08-07 17:01:47 +0300

    Small changes

--- src/main/java/com/backend/tes/domain/Product.java
+++ src/main/java/com/backend/tes/domain/Product.java
@@ -1,5 +1,7 @@
 package com.backend.tes.domain;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
@@ -8,13 +10,14 @@ import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.Timestamp;
 
-@Table(name = "product")
+@Table(name="product")
 @Data
 @Builder
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 @NoArgsConstructor
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Timestamp createdOn;
     private Timestamp modifiedOn;
